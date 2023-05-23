bd2505676ec8eaca803f6ac6ea930ce6c7a44c3d
afisman <afisman@gmail.com>
2023-05-23 19:29:31 +0200

    solved issues with barchart

--- src/components/layout/BarChart.jsx
+++ src/components/layout/BarChart.jsx
@@ -11,64 +11,49 @@ ChartJS.register(
     Legend
 )
 
-const BarChart = ({ userCycles }) => {
-    const padTo2Digits = (num) => {
-        return num.toString().padStart(2, '0')
-    }
+const BarChart = ({ dateArr, moodArr, flowArr }) => {
 
-    const getDayAndMonth = date => {
-        const date2 = new Date(date)
-        const year = date2.getFullYear()
-        const month = padTo2Digits(date2.getMonth() + 1)
-        const day = padTo2Digits(date2.getDate())
 
-        return [day, month, year].join('/')
-    }
 
-    const [userData, setUserData] = useState({
-        labels: userCycles.map((data) => getDayAndMonth(data.startDate)),
-        datasets: [{
-            label: "Mood",
-            data: userCycles.map((data) => data.mood),
-            backgroundColor: 'aqua',
-            borderColor: 'black',
-            borderWidth: 1
-        }
-            ,
-        {
-            label: "Flow",
-            data: userCycles.map((data) => data.flow),
-            backgroundColor: 'green',
-            borderColor: 'black',
-            borderWidth: 1
-        }
-        ]
-    })
 
-    console.log(userCycles.map((data) => data.flow))
-
-    return (
-        <div className='barChart' style={{ width: 700 }}>
+    const userData = {
+        labels: dateArr,
+        datasets: [
             {
-                userCycles ?
-                    <Bar
-                        data={userData}
-                        options={{
-                            barPercentage: 4,
-                            mantainAspectRatio: false,
-                            scales: {
-                                x: {
-                                    stacked: false
-                                },
-                                y: {
-                                    stacked: false
-                                }
-                            }
-                        }}
-                    />
-                    : <Loader />
+                label: "Mood",
+                data: moodArr,
+                backgroundColor: 'aqua',
+                borderColor: 'black',
+                borderWidth: 1,
+                stack: 'Stack 0'
+            }
+            ,
+            {
+                label: "Flow",
+                data: flowArr,
+                backgroundColor: 'green',
+                borderColor: 'black',
+                borderWidth: 1,
+                stack: 'Stack 1'
 
             }
+        ]
+    }
+
+    return (
+        <div className='barChart' style={{ width: 700, padding: '100px' }}>
+
+            <Bar
+                data={userData}
+                options={{
+                    responsive: true,
+                    mantainAspectRatio: false,
+
+                }}
+            />
+
+
+
         </div >
     )
 }
--- src/components/pages/ChartPage.jsx
+++ src/components/pages/ChartPage.jsx
@@ -8,15 +8,34 @@ import Loader from '../layout/Loader'
 const ChartPage = () => {
     const { user } = useContext(AuthContext)
     const [userCycles, setUserCycles] = useState([])
+    const [flowArr, setFlowArr] = useState([])
+    const [moodArr, setMoodArray] = useState([])
+    const [dateArr, setDateArr] = useState([])
     const { user_id } = useParams()
+
+    const padTo2Digits = (num) => {
+        return num.toString().padStart(2, '0')
+    }
+
+    const getDayAndMonth = date => {
+        const date2 = new Date(date)
+        const year = date2.getFullYear()
+        const month = padTo2Digits(date2.getMonth() + 1)
+        const day = padTo2Digits(date2.getDate())
+
+        return [day, month, year].join('/')
+    }
+
 
 
     const loadUser = () => {
         userService
             .getUser(user_id)
             .then(user => {
-                console.log(user)
-                setUserCycles(user.data.cycles)
+                console.log(typeof user.data.cycles[0].startDate)
+                const sortedCycles = user.data.cycles.sort((a, b) => Date.parse(a.startDate) - Date.parse(b.startDate))
+                console.log(sortedCycles)
+                setUserCycles(sortedCycles)
             })
             .catch(err => {
                 console.log(err)
@@ -27,12 +46,22 @@ const ChartPage = () => {
         loadUser()
     }, [])
 
+    useEffect(() => {
+        const fArr = userCycles.map((data) => data.flow)
+        const mArr = userCycles.map((data) => data.mood)
+        const dArr = userCycles.map((data) => getDayAndMonth(data.startDate))
+        setFlowArr(fArr)
+        setMoodArray(mArr)
+        setDateArr(dArr)
+
+    }, [userCycles])
+
     return (
         <>
             <div>ChartPage</div>
             {
                 userCycles ?
-                    <BarChart userCycles={userCycles} />
+                    <BarChart dateArr={dateArr} moodArr={moodArr} flowArr={flowArr} />
                     :
                     <Loader />
 
